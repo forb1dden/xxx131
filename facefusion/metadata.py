64521c0b5b1e8e1cecdf83964449466cb307ff09
Acces Forbidden <accesforbidden@gmail.com>
2023-11-03 23:04:21 +0700

    Add files via upload

---
+++ facefusion/metadata.py
@@ -0,0 +1,13 @@
+METADATA =\
+{
+	'name': 'PRIVATE SOFTWARE AI',
+	'description': 'Next generation face swapper and enhancer',
+	'version': '1.3.1',
+	'license': 'MIT',
+	'author': 'Henry Ruhs',
+	'url': ''
+}
+
+
+def get(key : str) -> str:
+	return METADATA[key]
